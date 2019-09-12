62ed0f45b338ac38c9801a24111b24c85178e0c0
Jeffrey Hoglen <jeffhoglen@gmail.com>
2019-09-12 19:44:56 -0400

    test

--- src/components/Reviewed.js
+++ src/components/Reviewed.js
@@ -10,7 +10,7 @@ class Reviewed extends React.Component {
   goReview= props =>{
     console.log("going to main page");
     window.location.href="./MainPage";
-  
+
   }
 
    handleSubmit=e=> {
@@ -18,40 +18,40 @@ class Reviewed extends React.Component {
       "Content-Type": "application/json",
       Accept: "application/json"
     };
-   
+
     let UserName = e.currentTarget.UserName.value;
     let Date= e.currentTarget.Date.value;
     let BrewName= e.currentTarget.BrewName.value;
     let BreweryName= e.currentTarget.BreweryName.value;
     let BrewStyle= e.currentTarget.BrewStyle.value;
     let Review= e.currentTarget.Review.value;
-    
-   
+
+   console.log('test')
 
     return fetch(`${API_BASE_URL}brewery`, {
       method: "POST",
       headers,
       body: JSON.stringify({
 
-       
+
         User: UserName,
         Date: Date,
         Brew: BrewName,
         Brewery: BreweryName,
         Style: BrewStyle,
         Review: Review,
-    
+
        })
     })
-      
+
       .then(res => {
         return res.json();
       })
       .then(res=>  {
-        
+
       window.location.href="./MainPage"
       })
-    
+
             .catch(err => {
         let message;
         if (err.code === 422) {
@@ -63,12 +63,12 @@ class Reviewed extends React.Component {
         }
         return message
 
-       
-       
+
+
       });
     };
-  
-   
+
+
 
 dateRef = React.createRef();
 usernameRef = React.createRef();
@@ -80,71 +80,68 @@ reviewRef = React.createRef();
 
 
 createBrews = event =>{
-  
-  event.preventDefault();  
- 
+
+  event.preventDefault();
+
  console.log('this.breweryRef.input.value',this.brewRef.current.value);
   const brew ={
-  
-    User: this.usernameRef.current.value, 
+
+    User: this.usernameRef.current.value,
     Date: this.dateRef.current.value,
     Brew: this.brewRef.current.value,
     Brewery: this.breweryRef.current.value,
     Style: this.styleRef.current.value,
     Review: this.reviewRef.current.value,
-    
-    
-   
+
+
+
   }
-  
+
   console.log(brew);
-  
-  
- 
-  
+
+
+
+
 };
 
 
     render(){
-       
+
 
       return (
-     
-     
+
+
      <div className="brewing">
         <Logout/>
-        
-            
+
+
         <main className="brews" onSubmit={this.createBrews}>
         <form className="brews" onSubmit={this.handleSubmit}>
-       
-  
+
+
         <h3 className="rate">Review your brew</h3>
         <input name="UserName" ref={this.usernameRef} type="text"placeholder="First Name Last Name" required/><br/><br/>
         <input name="Date" ref={this.dateRef} type="text"placeholder="Month/Day/Year" required/><br/><br/>
         <input name="BrewName" ref={this.brewRef} type="text"placeholder="Brew Name" required/><br/><br/>
       <input name="BreweryName" ref={this.breweryRef} type="text"placeholder="Brewery Name"required/> <br/><br/>
       <input name="BrewStyle" ref={this.styleRef} type="text"placeholder="Brew Style"required/><br/><br/>
-      
+
       <textarea name="Review" ref={this.reviewRef} placeholder="Review"required/>  <br/><br/>
-    
+
       <button className="about" type="submit" >Submit</button><br/><br/>
-     
-      <button className="about"  onClick={this.goReview} >Go to Reviews</button>  
-       
+
+      <button className="about"  onClick={this.goReview} >Go to Reviews</button>
+
       &nbsp;&nbsp;
-        
+
          </form>
          </main>
-       
-        
+
+
          </div>
-    
+
     );
   }
   }
-  
-  export default Reviewed;
 
-  
-  
+  export default Reviewed;
